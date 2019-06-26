680476bf68ae04e30ec7dd6fea917874fc2a9768
DJ Zaragoza <zaragoza0126@gmail.com>
2019-06-26 00:06:41 -0700

    still working on speech recognition functionality

--- SpeechRecognition.js
+++ SpeechRecognition.js
@@ -27,3 +27,57 @@ const dictate = () => {
         paragraph.textContent = speechToText;
     }
 }
+
+const synth = window.speechSynthesis;
+
+const speak = (action) => {
+    utterThis = new SpeechSynthesisUtterance(action());
+    synth.speak(utterThis);
+};
+
+const dictate = () => {
+    ... 
+    if (event.results[0].isFinal) {
+        if (speechToText.includes('what is the time')) {
+            speak(getTime);
+        };
+        if (speechToText.includes('what is today\'s date')) {
+            speak(getDate);
+        };
+        if (speechToText.includes('what is the weather in')) {
+            getTheWeather(speechToText);
+        };
+    }
+
+    ... 
+}
+
+const getTime = () => {
+    const time = new Date(Date.now());
+    return `the time is ${time.toLocaleString('en-US', {
+        hour: 'numeric', minute: 'numeric', hour12: true 
+    })}` 
+};
+
+const getDate = () => {
+    const time = new Date(Date.now())
+    return `today is ${time.toLocaleDateString()}`;
+};
+
+const getTheWeather = (speech) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${speech.split(' ')
+    [5]}&appid=58b6f7c78582bffab3936dac99c31b25&units=metric`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(weather){
+        if(weather.cod === '404') {
+            utterThis = new SpeechSynthesisUtterance (`I cannot find the weather for ${speech.split(' ')[5]}`);
+            synth.speak(utterThis);
+            return;
+        }
+        utterThis = new SpeechSynthesisUtterance(`the weather condition in ${weather.name} is mostly full of ${weather.weather[0].description}
+        at a temperature of ${weather.main.temp} degrees Celcius`);
+        synth.speak(utterThin);
+    });
+};
